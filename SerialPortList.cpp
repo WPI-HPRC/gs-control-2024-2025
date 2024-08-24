932297dcba7c2f221c29b338b7a6979826df0e9a
Will Scheirey <williamscheirey@gmail.com>
2024-08-24 18:23:43 -0400

    added ability to choose serial port from menu

---
+++ SerialPortList.cpp
@@ -0,0 +1,35 @@
+//
+// Created by William Scheirey on 8/24/24.
+//
+
+#include "SerialPortList.h"
+#include "Backend/Backend.h"
+#include <iostream>
+
+SerialPortList::SerialPortList(QWidget *parent) : QListWidget(parent)
+{
+    Backend &backend = Backend::getInstance();
+
+    connect(&backend, SIGNAL(foundSerialPorts(QList<QSerialPortInfo>)), this, SLOT(serialPortsFound(const QList<QSerialPortInfo> &)));
+    connect(this, SIGNAL(itemDoubleClicked(QListWidgetItem *)), this, SLOT(portChosen(QListWidgetItem *)));
+}
+
+void SerialPortList::serialPortsFound(const QList<QSerialPortInfo>& ports)
+{
+    for(const QSerialPortInfo& port : ports)
+    {
+        if(port.portName().contains("Bluetooth") || port.portName().contains("debug-console"))
+        {
+            continue;
+        }
+
+        this->serialPorts.append(port);
+        std::cout << port.portName().toStdString() << std::endl;
+        this->addItem(port.portName());
+    }
+}
+
+void SerialPortList::portChosen(QListWidgetItem *item)
+{
+    Backend::getInstance().connectToModule(serialPorts.at(indexFromItem(item).row()).portName(), Serving);
+}
