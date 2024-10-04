1186711da86b81d7c69da5fb694bf90c1ddd40aa
Nic DeBruin <nzdebruin@gmail.com>
2024-10-03 20:00:45 -0400

    Date & ground time from system RTC works. Implementing rocket + payload uptimes soon

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QApplication>
 #include "Backend/Backend.h"
 #include "Frontend/Windows/RadioControlsWindow/RadioControlsWindow.h"
+#include "Frontend/Windows/InfoWindow/timetestwindow.h"
 
 struct TestStruct
 {
@@ -16,11 +17,17 @@ int main(int argc, char *argv[])
 
     Backend &backend = Backend::getInstance();
 
-    RadioControlsWindow radioControlsWindow;
-    radioControlsWindow.showNormal();
-    radioControlsWindow.update();
-    radioControlsWindow.setWindowTitle("Radio Controls");
-    
+    // RadioControlsWindow radioControlsWindow;
+    // radioControlsWindow.showNormal();
+    // radioControlsWindow.update();
+    // radioControlsWindow.setWindowTitle("Radio Controls");
+
+    TimeTestWindow time_test_window;
+    time_test_window.showNormal();
+    time_test_window.update();
+    time_test_window.setWindowTitle("Test Window");
+
+
 
     TestStruct test;
     test.address = 0x0013A200423F474C;
