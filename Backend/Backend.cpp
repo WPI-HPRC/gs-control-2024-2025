838376c527ae0e15ee4b8589be33bcc18948502b
Nic DeBruin <nzdebruin@gmail.com>
2024-10-21 14:32:56 -0400

    Solved vagueness in a boolean statement.

--- Backend/Backend.cpp
+++ Backend/Backend.cpp
@@ -263,7 +263,7 @@ void Backend::receiveTelemetry(Backend::Telemetry telemetry)
     emit telemetryAvailable(telemetry);
 
     if(groundFlightTime.isValid() // if we haven't started the launch-elapsed timer
-    && telemetry.data.rocketData->state > 0) // and we're in a non-prelaunch state
+    && (telemetry.data.rocketData->state > 0)) // and we're in a non-prelaunch state
     {
         groundFlightTime.start(); // start a timer within the application
         rocketTimestampStart = telemetry.data.rocketData->timestamp; // get our start value for rocket time
