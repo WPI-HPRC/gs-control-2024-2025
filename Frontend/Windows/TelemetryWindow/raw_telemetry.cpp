f6165bdf3cc7354cceff11c4236e4614c372bec8
RafaelM244 <ep291ic@gmail.com>
2024-11-19 21:48:07 -0500

    Updated Raw_Telemetry
    Updated Raw_Telemetry

--- Frontend/Windows/TelemetryWindow/raw_telemetry.cpp
+++ Frontend/Windows/TelemetryWindow/raw_telemetry.cpp
@@ -8,19 +8,84 @@
 #include <QLabel>
 #include <QTableWidget>
 #include "ui_Raw_Telemetry.h"
+#include <QSerialPort>
 
+#include "Backend/Backend.h"
 
 Raw_Telemetry::Raw_Telemetry(QWidget *parent) :
-    QMainWindow(parent), ui(new Ui::Raw_Telemetry) {
+    QMainWindow(parent), ui(new Ui::Raw_Telemetry)
+{
     ui->setupUi(this);
-
-    int myInt = 150;
-
-    auto *label = new QLabel(QString::asprintf("%d", myInt));
-    QTableWidget *widget = ui->Column_1;
-    widget->setCellWidget(1, 1, label);
+    connect (&Backend::getInstance(), &Backend::telemetryAvailable, this, Raw_Telemetry::telemetryAvailable);
 }
-
 Raw_Telemetry::~Raw_Telemetry() {
     delete ui;
 }
+
+void Raw_Telemetry::telemetryAvailable(Backend::Telemetry telemetry){
+    if(telemetry.packetType== GroundStation::Rocket){
+    GroundStation::RocketTelemPacket *data = telemetry.data.rocketData;
+        processTelemetryData(*data);
+    }
+    else if(telemetry.packetType== GroundStation::Payload){
+    GroundStation::PayloadTelemPacket *data = telemetry.data.payloadData;}
+}
+
+void Raw_Telemetry::processTelemetryData(const GroundStation::RocketTelemPacket &data)
+{
+    updateTable(1, 1, QString::number(data.accelX));
+    /*
+    QList<QByteArray> telemetryValues = data.split(',');
+    if(telemetryValues.size() >= 30)
+    {
+        QVector<QPair<int, int>> tableMapping = {
+            {0,1},
+            {1,1},
+            {2,1},
+            {3,1},
+            {4,1},
+            {5,1},
+            {6,1},
+            {7,1},
+            {8,1},
+            {9,1},
+            {10,1},
+            {11,1},
+            {12,1},
+            {13,1},
+            {14,1},
+            {0,2},
+            {1,2},
+            {2,2},
+            {3,2},
+            {4,2},
+            {5,2},
+            {6,2},
+            {7,2},
+            {8,2},
+            {9,2},
+            {10,2},
+            {11,2},
+            {12,2},
+            {13,2},
+            {14,2},
+        };
+        //Loop through the mapping and update the table
+        for (int i=0; i<tableMapping.size(); i++){
+            int row = tableMapping[i].first;
+            int column = tableMapping[i].second;
+            QString value = telemetryValues.at(i);
+            updateTable(row,column,value);
+        }
+    }
+    */
+}
+
+
+void Raw_Telemetry::updateTable(int row, int column, const QString &value){
+    QTableWidget *widget = ui->Column_1;
+    auto *label = new QLabel(value);
+    widget->setCellWidget(row, column, label);
+}
+
+
--- Frontend/Windows/TelemetryWindow/raw_telemetry.h
+++ Frontend/Windows/TelemetryWindow/raw_telemetry.h
@@ -6,8 +6,11 @@
 #define RAW_TELEMETRY_H
 
 #include <QMainWindow>
+#include <QTableWidget>
+#include <QLabel>
+#include <QSerialPort>
 
-
+#include "Backend/Backend.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Raw_Telemetry; }
@@ -20,9 +23,14 @@ public:
     explicit Raw_Telemetry(QWidget *parent = nullptr);
     ~Raw_Telemetry() override;
 
+private slots:
+    void telemetryAvailable(Backend::Telemetry telemetry);
+
 private:
     Ui::Raw_Telemetry *ui;
+    QSerialPort *serialPort;
+    void updateTable(int row, int column, const QString &value); //function to update table cells
+    void processTelemetryData(const GroundStation::RocketTelemPacket &data); //function to process the incoming telemetry data
 };
-
 
 #endif //RAW_TELEMETRY_H
