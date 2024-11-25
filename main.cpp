896ba28f763e0c2f230383311d722cd454ec9207
Will Scheirey <williamscheirey@gmail.com>
2024-11-25 12:06:19 -0500

    added radio performance window to main window

--- main.cpp
+++ main.cpp
@@ -17,22 +17,6 @@ int main(int argc, char *argv[])
 
     Backend &backend = Backend::getInstance();
 
-//    RadioControlsWindow radioControlsWindow;
-//    radioControlsWindow.showNormal();
-//    radioControlsWindow.update();
-//    radioControlsWindow.setWindowTitle("Radio Controls");
-
-    
-    RadioPerformanceWindow radioPerformanceWindow;
-    radioPerformanceWindow.showNormal();
-    radioPerformanceWindow.update();
-    radioPerformanceWindow.setWindowTitle("Live Radio Performance Stats");
-
-//    Raw_Telemetry raw_telemetryWindow;
-//    raw_telemetryWindow.showNormal();
-//    raw_telemetryWindow.update();
-//    raw_telemetryWindow.setWindowTitle("Raw Telemetry");
-
     MainWindow mainWindow;
     mainWindow.showNormal();
     mainWindow.setWindowTitle("Main Window");
